35b839b0077836e35b8aacee1d616b3aed225a59
Elder Santos Silva <elderssilva1@gmail.com>
2024-11-21 03:11:52 -0300

    Sistema de Registros MVC
    Versão 1.0 utilizada no video de apresentação, contendo as mudanças solicitadas pela comunidade.
    
    Projeto realizado para atividade laboratorial da Estácio de Sá.

---
+++ Migrations/20241011221854_Inicial-criacao.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRUD_MVC_SQL.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicialcriacao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Produto",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Nome = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Descrição = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Produto", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Produto");
+        }
+    }
+}
---
+++ Models/Produto.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace CRUD_MVC_SQL.Models
+{
+
+    [Table ("Produto")]
+    public class Produto
+    {
+        [Column("Id")]
+        [Display(Name = "Código")]
+        public int ID { get; set; }
+
+        [Column("Nome")]
+        [Display(Name = "Nome")]
+        public string Nome { get; set; }
+
+        [Column("Descrição")]
+        [Display(Name = "Descrição")]
+        public string Descrição { get; set; }
+
+    }
+}
